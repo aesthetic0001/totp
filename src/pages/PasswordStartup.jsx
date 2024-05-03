5acf1ab9ce610302ea50d79c52a21af9d5146f2d
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-05-03 16:56:35 -0400

    start working on password encryptioN

--- src/pages/PasswordStartup.jsx
+++ src/pages/PasswordStartup.jsx
@@ -1,9 +1,18 @@
+import {useEffect, useState} from "react";
+import EcholessInput from "../components/EcholessInput.jsx";
+
 function PasswordStartup() {
-  return (
-      <div>
-          <h1>Password</h1>
-      </div>
-  );
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className="flex flex-col justify-center items-center text-center space-y-10">
+            <h1 className="text-2xl my-8">Please enter the master password to decrypt your OTP secrets</h1>
+            <EcholessInput currentText={"*".repeat(password.length)} placeholder="Master Password" onInput={(event) => {
+                if (event.target.value.length >= 32) return
+                setPassword(event.target.value);
+            }}></EcholessInput>
+        </div>
+    );
 }
 
 export default PasswordStartup;
