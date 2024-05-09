42833907d2ba14b90a6b7eeb258682b1359f1755
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-05-09 18:38:38 -0400

    settings menu buttons done

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,13 +1,18 @@
-// import React from "react";
-import TextInput from "../components/TextInput.jsx";
 import {ArrowLeft} from "lucide-react";
 import {Link} from "react-router-dom";
 import {toast, Toaster} from "react-hot-toast";
-import { save, open } from '@tauri-apps/api/dialog';
-import { writeTextFile } from '@tauri-apps/api/fs';
-import { invoke } from '@tauri-apps/api/tauri';
+import {save, open} from '@tauri-apps/api/dialog';
+import {writeTextFile} from '@tauri-apps/api/fs';
+import {invoke} from '@tauri-apps/api/tauri';
+import {useState} from "react";
 
 function Settings() {
+    const [passwordProtected, setPasswordProtected] = useState(false);
+
+    invoke('is_encrypted').then((res) => {
+        setPasswordProtected(res)
+    })
+    
     return (
         <div>
             <div className="flex flex-col space-y-10 justify-center items-center text-center">
@@ -16,40 +21,46 @@ function Settings() {
                     <ArrowLeft className="h-8 w-8"/>
                     <div className="text-4xl font-bold">Settings</div>
                 </Link>
-                <div className="flex space-x-4">
-                </div>
-                <div className="flex flex-row space-x-5">
+                <div class="relative">
                     <button
-                        className="bg-emerald-400 hover:bg-emerald-500 transition-all ease-in-out text-white font-bold py-2 px-4 rounded-full"
+                        className={(!passwordProtected ? "bg-emerald-400 hover:bg-emerald-500 " : "bg-red-400 hover:bg-red-500 ") + "transition-all ease-in-out text-white font-bold py-2 px-4 rounded-full"}
                         onClick={async () => {
-                            const selected = await open({
-                                filters: [{
-                                    name: 'Import from JSON',
-                                    extensions: ['json']
-                                }]
+                        }}>
+                        {
+                            !passwordProtected ? "Enable Password Encryption" : "Disable Password Encryption"
+                        }
+                    </button>
+                </div>
+                <button
+                    className="bg-emerald-400 hover:bg-emerald-500 transition-all ease-in-out text-white font-bold py-2 px-4 rounded-full"
+                    onClick={async () => {
+                        const selected = await open({
+                            filters: [{
+                                name: 'Import from JSON',
+                                extensions: ['json']
+                            }]
+                        });
+                        if (selected) {
+                            // import selected file
+                            const imported_count = await invoke('import_from_json', {path: selected});
+                            toast.success(`Imported ${imported_count} accounts!`, {
+                                duration: 2000
                             });
-                            if (selected) {
-                                // import selected file
-                                const imported_count = await invoke('import_from_json', {path: selected});
-                                toast.success(`Imported ${imported_count} accounts!`, {
-                                    duration: 2000
-                                });
-                            }
-                        }}>
-                        Import Accounts
-                    </button>
-                    <button
-                        className="bg-emerald-400 hover:bg-emerald-500 transition-all ease-in-out text-white font-bold py-2 px-4 rounded-full"
-                        onClick={async () => {
-                            const filePath = await save({defaultPath: "exported-accounts.json"});
-                            if (filePath) {
-                                const exports = await invoke('export_to_json');
-                                await writeTextFile(filePath, exports)
-                            }
-                        }}>
-                        Export Accounts
-                    </button>
-                </div>
+                        }
+                    }}>
+                    Import Accounts
+                </button>
+                <button
+                    className="bg-emerald-400 hover:bg-emerald-500 transition-all ease-in-out text-white font-bold py-2 px-4 rounded-full"
+                    onClick={async () => {
+                        const filePath = await save({defaultPath: "exported-accounts.json"});
+                        if (filePath) {
+                            const exports = await invoke('export_to_json');
+                            await writeTextFile(filePath, exports)
+                        }
+                    }}>
+                    Export Accounts
+                </button>
             </div>
             <Toaster position="bottom-center"/>
         </div>
