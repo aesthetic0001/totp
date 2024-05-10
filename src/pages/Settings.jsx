f8ec1edef4bfc0cfff5cf9181daa00ed9484148f
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-05-09 20:27:53 -0400

    added menu for setting passwords + modal component

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -5,14 +5,17 @@ import {save, open} from '@tauri-apps/api/dialog';
 import {writeTextFile} from '@tauri-apps/api/fs';
 import {invoke} from '@tauri-apps/api/tauri';
 import {useState} from "react";
+import Modal from "../components/Modal.jsx";
 
 function Settings() {
     const [passwordProtected, setPasswordProtected] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [password, setPassword] = useState('');
 
     invoke('is_encrypted').then((res) => {
         setPasswordProtected(res)
     })
-    
+
     return (
         <div>
             <div className="flex flex-col space-y-10 justify-center items-center text-center">
@@ -21,10 +24,11 @@ function Settings() {
                     <ArrowLeft className="h-8 w-8"/>
                     <div className="text-4xl font-bold">Settings</div>
                 </Link>
-                <div class="relative">
+                <div className="relative">
                     <button
                         className={(!passwordProtected ? "bg-emerald-400 hover:bg-emerald-500 " : "bg-red-400 hover:bg-red-500 ") + "transition-all ease-in-out text-white font-bold py-2 px-4 rounded-full"}
-                        onClick={async () => {
+                        onClick={() => {
+                            setModalOpen(true);
                         }}>
                         {
                             !passwordProtected ? "Enable Password Encryption" : "Disable Password Encryption"
@@ -62,6 +66,23 @@ function Settings() {
                     Export Accounts
                 </button>
             </div>
+            <Modal open={modalOpen} setOpen={setModalOpen}
+                   title={!passwordProtected ? 'Set Master Password' : 'Disable Master Password'}
+                   description={!passwordProtected ? "Please note that whitespaces will be excluded during encryption!" : "Your files will be stored in plaintext if this option is disabled!"}
+                   actionText="Confirm" dialogueBox={!passwordProtected} onDialogAction={async (x) => {
+                const previous = password
+                setPassword(x);
+                if (!x) return false
+                if (x.length > 32) {
+                    if (previous.length < x.length && (x.length - 33) % 8 === 0) {
+                        toast.error("Password cannot be longer than 32 characters!", {
+                            duration: 2000
+                        });
+                    }
+                    return false
+                }
+                return true
+            }}/>
             <Toaster position="bottom-center"/>
         </div>
     );
